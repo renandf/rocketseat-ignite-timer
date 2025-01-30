f1e452f1d2263bbd35cb9eeea1be4e6ad2d6cb27
Renan Castro <renandf@gmail.com>
2025-01-30 20:35:20 +1000

    feat: Add Styled Components and some global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Button } from './components/Button'
+
 export function App() {
 
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Button />
+    </ThemeProvider>
   )
 }
